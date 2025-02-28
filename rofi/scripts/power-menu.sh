801955a6701cd56915dfd983ccbaf9d945c3208d
Meinard Edrei <meinardsantoss@gmail.com>
2025-02-28 16:06:39 +0800

    Added power menu gui and themes for rofi

---
+++ rofi/scripts/power-menu.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Ensure the script is executable
+chmod +x ~/.config/rofi/scripts/power-menu.sh
+
+chosen=$(printf "󰐥  Power Off\n   Restart\n  Suspend\n   Hibernate\n󰍃  Log Out\n   Lock" | \
+    rofi -dmenu -i -p "" -markup-rows -hide-scrollbar \
+    -disable-history -kb-text-clear "" -kb-remove-to-eol "" \
+    -kb-remove-word-back "" -kb-remove-word-forward "" \
+    -kb-accept-entry "Return" \
+    -theme ~/.config/rofi/themes/power-menu.rasi)
+
+case "$chosen" in
+    "󰐥  Power Off") tmux kill-server & poweroff ;;
+    "  Restart") tmux kill-server & reboot ;;
+    "  Suspend") systemctl suspend && hyprlock ;;
+    "  Hibernate") systemctl hibernate ;;
+    "󰍃  Log Out") hyprctl dispatch exit ;;
+    "  Lock") betterlockscreen -l ;;
+    *) exit 1 ;;
+esac
