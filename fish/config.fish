c9adddbc741b44f939bae3df4a024a76f2ececcd
Meinard Edrei <meinardsantoss@gmail.com>
2025-04-30 13:52:55 +0800

    Added config for screenshots

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,9 @@ end
 
 starship init fish | source
 set -Ux HYPRSHOT_DIR "$HOME/Pictures/Screenshots"
+
+# cmdline-tools path
+set -g PATH $HOME/Android/Sdk/cmdline-tools/latest/bin $HOME/Android/Sdk/platform-tools $HOME/Android/Sdk/emulator $PATH
+
+# flutter
+set -U fish_user_paths /opt/flutter/bin $fish_user_paths
