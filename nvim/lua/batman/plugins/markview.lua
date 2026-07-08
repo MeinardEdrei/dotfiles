c413e6b40dd0344d6230371e45110ff358bf46f1
MeinardEdrei <meinardsantoss@gmail.com>
2026-07-08 17:00:01 +0800

    added new plugin for markdown files

---
+++ nvim/lua/batman/plugins/markview.lua
@@ -0,0 +1,9 @@
+return {
+	"OXY2DEV/markview.nvim",
+	lazy = false,
+	keys = {
+		{ "<leader>mp", "<cmd>Markview Toggle<cr>", desc = "Toggle Markdown Preview" },
+		{ "<leader>mh", "<cmd>Markview HybridToggle<cr>", desc = "Toggle Markdown Hybrid Mode" },
+		{ "<leader>ms", "<cmd>Markview splitToggle<cr>", desc = "Toggle Markdown Splitview" },
+	},
+}
